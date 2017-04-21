6b10a94d4970c4be0b14924c81b62dfdb66a4d32
Mike <mike@miketruax.com>
2017-04-21 14:57:16 -0500

    Finishes controllers to components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import {HomeComponent} from "./home/home.component";
 import {SkillsComponent} from "./skills/skills.component"
 import {PortfolioComponent} from "./portfolio/portfolio.component";
 import {MovieSearchComponent} from "./portfolio/movie-search/movie-search.component";
+import {TicTacToeComponent} from "./portfolio/tic-tac-toe/tictactoe.component";
 export const routes: Routes = [
   { path: '',   component: HomeComponent },
   { path: 'food',  component: FoodComponent},
   { path: 'about', component: AboutComponent},
   { path: 'skills', component: SkillsComponent },
   { path: 'portfolio', component: PortfolioComponent},
-  { path: 'movie', component: MovieSearchComponent},
+  { path: 'portfolio/movie-search', component: MovieSearchComponent},
+  { path: 'portfolio/tic-tac-toe', component: TicTacToeComponent},
   { path: '**',   redirectTo: '/', pathMatch: 'full' }
 ];

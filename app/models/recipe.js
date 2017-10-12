f54d7c881ef0b7d91f0da2357b2b1ad5104c89a2
Mike <mike@miketruax.com>
2017-10-12 12:15:35 -0500

    Adds API endpoints for all CRUD

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,62 +1,62 @@
 import {acquire} from '../connection';
 
 
-      function get(res) {
-        acquire((con) => {
-          con.query('select * from recipes', function (err, result) {
-            con.release();
-            res.send(result);
-          });
-        })
-      };
+function getRecipes(res) {
+  acquire((con) => {
+    con.query('select * from recipes', function (err, result) {
+      con.release();
+      res.send(result);
+    });
+  })
+}
 
-    function cat(id, res) {
-      acquire((con) => {
-            con.query('select * from recipes where category_ID = ?', [id], function (err, result) {
-                con.release();
-                res.send(result);
-            });
-        });
-    };
+function getCategories(id, res) {
+  acquire((con) => {
+    con.query('select * from recipes where category_ID = ?', [id], function (err, result) {
+      con.release();
+      res.send(result);
+    });
+  });
+}
 
 
-    function create(recipe, res) {
-      acquire((con) => {
-            con.query('insert into recipes set ?', recipe, function (err, result) {
-                con.release();
-                if (err) {
-                    res.send({status: 1, message: 'Recipe creation failed'});
-                } else {
-                    res.send({status: 0, message: 'Recipe created successfully'});
-                }
-            });
-        });
-    };
+function createRecipe(recipe, res) {
+  acquire((con) => {
+    con.query('insert into recipes set ?', recipe, function (err, result) {
+      con.release();
+      if (err) {
+        res.send({status: 1, message: 'Recipe creation failed'});
+      } else {
+        res.send({status: 0, message: 'Recipe created successfully'});
+      }
+    });
+  });
+}
 
-    function update(recipe, res) {
-      acquire((con) => {
-            con.query('update recipe set ? where id = ?', [recipe, recipe.id], function (err, result) {
-                con.release();
-                if (err) {
-                    res.send({status: 1, message: 'recipe update failed'});
-                } else {
-                    res.send({status: 0, message: 'recipe updated successfully'});
-                }
-            });
-        });
-    };
+function updateRecipe(recipe, res) {
+  acquire((con) => {
+    con.query('update recipe set ? where id = ?', [recipe, recipe.id], function (err, result) {
+      con.release();
+      if (err) {
+        res.send({status: 1, message: 'recipe update failed'});
+      } else {
+        res.send({status: 0, message: 'recipe updated successfully'});
+      }
+    });
+  });
+}
 
-    function del(id, res) {
-      acquire((con) => {
-            con.query('delete from recipe where id = ?', [id], function (err, result) {
-                con.release();
-                if (err) {
-                    res.send({status: 1, message: 'Failed to delete'});
-                } else {
-                    res.send({status: 0, message: 'Deleted successfully'});
-                }
-            });
-        });
-    };
+function deleteRecipe(id, res) {
+  acquire((con) => {
+    con.query('delete from recipe where id = ?', [id], function (err, result) {
+      con.release();
+      if (err) {
+        res.send({status: 1, message: 'Failed to delete'});
+      } else {
+        res.send({status: 0, message: 'Deleted successfully'});
+      }
+    });
+  });
+}
 
-    export {get, cat, create, update, del}
+export {getRecipes, getCategories, createRecipe, updateRecipe, deleteRecipe}

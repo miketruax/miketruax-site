1b756d346f06e7f5abefea988e2bec1995464ec5
Mike <mike@miketruax.com>
2018-05-31 13:44:07 -0500

    Fixes minor styling and updates buttons

--- src/app/portfolio/periodic-table/table/table.component.ts
+++ src/app/portfolio/periodic-table/table/table.component.ts
@@ -1,12 +1,12 @@
-import {Component, HostListener} from '@angular/core';
-import {DomSanitizer} from "@angular/platform-browser";
+import {Component, HostListener, OnInit} from '@angular/core';
+import {DomSanitizer, SafeHtml} from "@angular/platform-browser";
 
 @Component({
   selector: 'table-component',
   templateUrl: './table.component.html',
   styleUrls: ['./table.component.scss']
 })
-export class TableComponent {
+export class TableComponent implements OnInit{
   moreInfo: boolean = false;
   showRanking: boolean = false;
   style: string = "classic";
@@ -15,11 +15,22 @@ export class TableComponent {
   selectedElement: Object = {};
   currYear: number;
   year: number;
+  octicons: any = require('octicons');
+  globeIcon: SafeHtml;
+  rocketIcon: SafeHtml;
+  calendarIcon: SafeHtml;
+  beakerIcon: SafeHtml;
+  humanIcon: SafeHtml;
+  clockIcon: SafeHtml;
+  listorderedIcon: SafeHtml;
   constructor(private sanitizer: DomSanitizer){
     this.elements = require('../assets/elem_v3.json');
     this.currYear = new Date().getFullYear();
     this.year = this.currYear;
   }
+
+
+
   clearCounter(){
     clearInterval(this.counter);
   }
@@ -50,5 +61,17 @@ export class TableComponent {
 
   ranking(){
     this.showRanking = !this.showRanking;
+  }
+
+  ngOnInit(){
+
+    this.calendarIcon =  this.sanitizer.bypassSecurityTrustHtml(this.octicons.calendar.toSVG({height: "35", width: "35", "class": "svgIcon"}) + "<p class='iconDescription'>Discovery Date</p>");
+    this.globeIcon =  this.sanitizer.bypassSecurityTrustHtml(this.octicons.globe.toSVG({height: "35", width: "35", "class": "svgIcon"}) + "<p class='iconDescription'>Crust Abundance</p>");
+    this.rocketIcon =  this.sanitizer.bypassSecurityTrustHtml(this.octicons.rocket.toSVG({height: "35", width: "35", "class": "svgIcon"}) + "<p class='iconDescription'>Universe Abundance</p>");
+    this.beakerIcon =  this.sanitizer.bypassSecurityTrustHtml(this.octicons.beaker.toSVG({height: "35", width: "35", "class": "svgIcon"}) + "<p class='iconDescription'>Standard View</p>");
+    this.humanIcon =  this.sanitizer.bypassSecurityTrustHtml(this.octicons.person.toSVG({height: "35", width: "35", "class": "svgIcon"}) + "<p class='iconDescription'>Human Abundance</p>");
+    this.clockIcon =  this.sanitizer.bypassSecurityTrustHtml("<h3>"+this.octicons.clock.toSVG({height: "35", width: "35", "class": "svgIcon"}) + " View By Chronological Order");
+    this.listorderedIcon =  this.sanitizer.bypassSecurityTrustHtml("<h3>"+this.octicons["list-ordered"].toSVG({height: "35", width: "35", "class": "svgIcon"}) + " View Ranked List");
+
   }
 }

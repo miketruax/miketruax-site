6be4c007490f16094e448e0521f40d8d151e7a0d
Mike <mike@miketruax.com>
2018-10-25 14:21:25 -0500

    finalizes transition for app.component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,7 +3,8 @@ import {fadeInAnimation} from "../animations/fade-in.animation";
 
 @Component({
   selector: 'home-component',
-  templateUrl: 'home.component.html'
+  templateUrl: 'home.component.html',
+  styleUrls: ['home.component.scss']
 })
 export class HomeComponent {
 
